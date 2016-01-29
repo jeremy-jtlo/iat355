4aba00e0deb0219cff43b4093d062b4ae09790ef
Jeremy L <y2kfirzen@gmail.com>
2016-01-29 14:36:51 -0800

    Converted CSV entries to integers

--- draw-graphs.js
+++ draw-graphs.js
@@ -1,11 +1,14 @@
-var csv_file = "http://www.sfu.ca/~jtlo/iat355/data/building-permits.csv";
+var csv_file = "http://www.sfu.ca/~jtlo/iat355/data/building-permits-trimmed.csv";
 
 d3.select("body").append("p").text("New paragraph!");
 
-d3.csv(csv_file, function(string) {
-    root = string;
-    update();
-}).row(function(d){
+d3.csv(csv_file)
+.row(function(d){
+	var year_marker = 1998;
+	for (year_marker; year_marker < 2015; year_marker++){
+		var year_string = year_marker.toString();
+		d[year_string] = +d[year_string]
+	}
     console.log(d);
 }).get(function(error, data){
     console.log(data);
