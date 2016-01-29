37a1e489b553e1a278f16d8e562a8f1c41a44ea1
Jeremy <y2kfirzen@gmail.com>
2016-01-29 13:54:21 -0800

    Saving some changes

--- draw-graphs.js
+++ draw-graphs.js
@@ -1,12 +1,14 @@
-var csv_file = "building-permits.csv";
+var csv_file = "http://www.sfu.ca/~jtlo/iat355/data/building-permits.csv";
 
 d3.select("body").append("p").text("New paragraph!");
 
 d3.csv(csv_file, function(string) {
     root = string;
     update();
-}).get(function(error, year){
-    console.log(year);
+}).row(function(d){
+    console.log(d);
+}).get(function(error, data){
+    console.log(data);
 });
 
 // d3.csv(csv_file).get(function(error, year){console.log(year)});
