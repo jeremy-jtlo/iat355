7c510f32dbeb35847db18586f829c3e3ffd54f85
Ernie Tsang <ernie_tsang@hotmail.com>
2016-03-21 00:21:14 -0700

    Pieces of paired bar chart code

---
+++ Assignment 6/js/HouseholdVis.js
@@ -0,0 +1,214 @@
+// d3.scale.domain([dates]) takes an array of date objects
+/*
+RESOURCES
+
+Sorting date objects:
+https://onpub.com/how-to-sort-an-array-of-dates-with-javascript-s7-a109
+
+Sorting objects by date key:
+http://stackoverflow.com/questions/19430561/how-to-sort-a-javascript-array-of-objects-by-date
+
+*/
+
+$( document ).ready(function() {
+    console.log( "Practice2.js is ready." );
+});
+
+var vis = d3.select("#visualisation"),
+    WIDTH = 1000,
+    HEIGHT = 500,
+    MARGINS = {
+        top: 20,
+        right: 20,
+        bottom: 20,
+        left: 50
+    };
+
+// Returns a list of date objects from input data
+function getDateRange(d){
+    var date_list = []
+
+    // Loop
+    for (var i=0; i<d.length; i++){
+        date_object = new Date(d[i]["Date"]);
+        // console.log(date_object);
+        date_list.push(date_object);
+    }
+
+    // console.log(date_list);
+    return date_list;
+}
+
+// Returns a list of entries matching a REGION and LISTING TYPE
+function returnDataSet(d, desired_region, desired_type){
+    var listing_list = []
+    
+    // Loop
+    for (var i=0; i<d.length; i++){
+        var current_listing = d[i];
+        if ((current_listing["Region"] == desired_region) && (current_listing["Type"] == desired_type)) {
+            listing_list.push(current_listing);
+        }
+    }
+    
+    return listing_list;
+}
+
+// Sort dates in ascending order when called from sort()
+var date_sort_asc = function (date1, date2) {
+    if (date1 > date2) return 1;
+    if (date1 < date2) return -1;
+    return 0;
+};
+
+// Sort objects by 'Date' key
+function sort_obj_by_date(a,b) { 
+    return new Date(a.Date).getTime() - new Date(b.Date).getTime()
+}
+
+// Cleaning csv file
+function fixDataRow(d) {
+    var format = d3.time.format("%Y-%m-%d");
+    d["Date"] = format.parse(d["Date"]);
+    d["Households"] = +d["Households"];
+
+    return d;
+}
+
+function setupFilters(svg, points){
+    // Listen to clicks on checkboxes
+    d3.selectAll("input[name='region_check']").on("click", function(){
+        drawLineGraph(svg, points);
+    });
+}
+
+// Drawing the graph
+function drawLineGraph(svg, points) {
+  
+    var date_array = getDateRange(points);
+    
+    date_array.sort(date_sort_asc);
+    // console.log(date_array)
+    
+    var start_date = date_array[0];
+    var end_date = date_array.pop()
+    
+    var north_apts = returnDataSet(points, "North", "Apartment");
+    north_apts.sort(sort_obj_by_date);
+
+    var central_apts = returnDataSet(points, "Central", "Apartment");
+    central_apts.sort(sort_obj_by_date);
+
+    var south_apts = returnDataSet(points, "South", "Apartment");
+    south_apts.sort(sort_obj_by_date);
+
+    var north_townh = returnDataSet(points, "North", "Townhouse");
+    north_townh.sort(sort_obj_by_date);
+    
+     var central_townh = returnDataSet(points, "Central", "Townhouse");
+    central_townh.sort(sort_obj_by_date);
+
+    var south_townh = returnDataSet(points, "South", "Townhouse");
+    south_townh.sort(sort_obj_by_date);
+
+     var north_det = returnDataSet(points, "North", "Detached");
+    north_det.sort(sort_obj_by_date);
+    
+     var central_det = returnDataSet(points, "Central", "Detached");
+    central_det.sort(sort_obj_by_date);
+
+    var south_det = returnDataSet(points, "South", "Detached");
+    south_det.sort(sort_obj_by_date);
+    
+    var xScale = d3.time.scale().domain([start_date, end_date]).range([MARGINS.left, WIDTH - MARGINS.right]);
+    
+    // Define axes
+    var xAxis = d3.svg.axis()
+            .scale(xScale);
+            
+    var yScale = d3.scale.linear()
+    .domain([0, d3.max(points, function(d) { return d.Households; })])
+    .range([HEIGHT - MARGINS.top - MARGINS.bottom, 0]);
+
+    var yAxis = d3.svg.axis()
+    .scale(yScale)
+    .orient('left')
+    .tickPadding(8);
+
+    // Append axes        
+    vis.append("svg:g")
+    .attr("class", "axis")
+    .attr("transform", "translate(0," + (HEIGHT - MARGINS.bottom) + ")")
+    .call(xAxis);
+    
+    vis.append("svg:g")
+    .attr("class", "axis")
+    .attr("transform", "translate(" + (MARGINS.left) + ",0)")
+    .call(yAxis);
+    
+    var lineGen = d3.svg.line()
+    .x(function(d) {
+        return xScale(d.Date);
+    })
+    .y(function(d) {
+        return yScale(d.Households);
+    })
+    .interpolate("monotone"); // Interpolate gives us curves
+    
+    // Append line to visualization
+    function visAppend(region, data_set, type, width) {
+        vis.append('svg:path')
+            .attr('class', region + ' data-line ' + type)
+            .attr('d', lineGen(data_set))
+            .attr('stroke-width', width)
+            .attr('fill', 'none');
+    }
+
+    // Add all lines to our svg
+    visAppend('North', north_apts, 'apt1', 2);
+    visAppend('Central', central_apts, 'apt2', 2);
+    visAppend('South', south_apts, 'apt3', 2);
+
+    visAppend('North', north_townh, 'townh1', 2);
+    visAppend('Central', central_townh, 'townh2', 2);
+    visAppend('South', south_townh, 'townh3', 2);
+
+    visAppend('North', north_det, 'det1', 2);
+    visAppend('Central', central_det, 'det2', 2);
+    visAppend('South', south_det, 'det3', 2);
+
+    if ($('#north').prop('checked') == true){
+        $('.North').show();
+        // console.log("NORTH");
+    } else {
+        $('.North').hide();
+        // console.log("NORTH");
+    }
+    if ($('#central').prop('checked') == true){
+        $('.Central').show();
+        // console.log("CENTRAL");
+    } else {
+        $('.Central').hide();
+        // console.log("CENTRAL");
+    }
+    if ($('#south').prop('checked') == true){
+        $('.South').show();
+        // console.log("SOUTH");
+    } else {
+        $('.South').hide();
+        // console.log("SOUTH");
+    }
+    
+}
+
+d3.csv("http://www.sfu.ca/~erniet/IAT355/ernie-tsang_jeremy-lo_A4/csv/Active_Listings_D3.csv")
+.row(fixDataRow)
+.get(function(error, points){
+    if(error){
+        console.error("An error happened while opening the file. "+ error);
+    }else{
+        var svg = d3.select("svg#visualisation ");
+        drawLineGraph(svg, points);
+        setupFilters(svg, points);
+    }
+});
---
+++ Assignment 6/js/HpiPopVis.js
@@ -0,0 +1,123 @@
+// https://jasonneylon.wordpress.com/2013/09/05/two-sided-horizontal-barchart-using-d3-js/
+
+$( document ).ready(function() {
+    console.log( "Practice2.js is ready." );
+});
+
+var vis = d3.select("#visualisation"),
+    WIDTH = 1000,
+    HEIGHT = 500,
+    MARGINS = {
+        top: 20,
+        right: 20,
+        bottom: 20,
+        left: 50
+    };
+
+function getDateRange(d){
+    var date_list = []
+
+    // Loop
+    for (var i=0; i<d.length; i++){
+        date_object = new Date(d[i]["Date"]);
+        // console.log(date_object);
+        date_list.push(date_object);
+    }
+
+    // console.log(date_list);
+    return date_list;
+}
+
+var date_sort_asc = function (date1, date2) {
+    if (date1 > date2) return 1;
+    if (date1 < date2) return -1;
+    return 0;
+};
+
+function sort_obj_by_date(a,b) { 
+    return new Date(a.Date).getTime() - new Date(b.Date).getTime()
+}
+
+function fixDataRow(d) {
+    var format = d3.time.format("%Y-%m-%d");
+    d["Date"] = format.parse(d["Date"]);
+    d["HPI"] = +d["HPI"];
+    d["Income"] = +d["Income"];
+
+    return d;
+}
+
+function drawPairedBars(svg, points) {
+
+    var labelArea = 160;
+
+    var chart,
+        width = 400,
+        bar_height = 20,
+        height = bar_height * (date_list.length);
+
+    var rightOffset = width + labelArea;
+
+    var chart = d3.select("body")
+        .append('svg')
+        .attr('class', 'chart')
+        .attr('width', labelArea + width + width)
+        .attr('height', height);
+
+    var xFrom = d3.scale.linear()
+        .domain([0, d3.max(points, function(d) { return d.HPI; })])
+        .range([HEIGHT - MARGINS.top - MARGINS.bottom, 0]);
+
+    var xTo = d3.scale.linear()
+        .domain([0, d3.max(points, function(d) { return d.Income; })])
+        .range([HEIGHT - MARGINS.top - MARGINS.bottom, 0]);
+
+    var yScale = d3.time.scale().domain([start_date, end_date]).range([MARGINS.left, WIDTH - MARGINS.right]);
+    
+    // Define axes
+    var yScale = d3.svg.axis()
+        .scale(yScale);
+
+    var yPosByIndex = function(d, index){ return y(index); } 
+
+    chart.selectAll("rect.left")
+        .data(d.HPI)
+        .enter().append("rect")
+        .attr("x", function(pos) { return width - xFrom(pos); })
+        .attr("y", yPosByIndex)
+        .attr("class", "left")
+        .attr("width", xFrom)
+        .attr("height", y.rangeBand()); 
+
+    chart.selectAll("text.date_list")
+        .data(date_list)
+        .enter().append("text")
+        .attr("x", (labelArea / 2) + width)
+        .attr("y", function(d){ return y(d) + y.rangeBand()/2; } )
+        .attr("dy", ".20em")
+        .attr("text-anchor", "middle")
+        .attr('class', 'date_list')
+        .text(String);
+
+    chart.selectAll("rect.right")
+        .data(d.Income)
+        .enter().append("rect")
+        .attr("x", rightOffset)
+        .attr("y", yPosByIndex)
+        .attr("class", "right")
+        .attr("width", xTo)
+        .attr("height", y.rangeBand()); 
+}
+
+
+d3.csv("http://www.sfu.ca/~erniet/IAT355/Assignment%206/csv/SurreyHPIIncome.csv")
+.row(fixDataRow)
+.get(function(error, points){
+    if(error){
+        console.error("An error happened while opening the file. "+ error);
+    }else{
+        var svg = d3.select("svg#visualisation ");
+        drawPairedBars(svg, points);
+        setupFilters(svg, points);
+    }
+});
