9611e0ea971953b7276298ace4b3be98141d0cdf
Ernie Tsang <ernie_tsang@hotmail.com>
2016-04-07 16:52:34 -0700

    attempts at Pop graph

--- final-submission/js/visualize.js
+++ final-submission/js/visualize.js
@@ -304,14 +304,134 @@ Draws a line graph with our population data
 
 function population(){
 
+    var line_weight = 2;
+    var population_ticks = 5;
+
     // HELPER FUNCTION: Clean our population csv.
-    function fixPopRow(){
-        // Convert things to numbers, dates, etc.
+    function fixPopRow(d)
+    {
+        var format = d3.time.format("%Y-%m-%d");
+        d["Date"] = format.parse(d["Date"]);
+        d["Households"] = +d["Households"];
+        return d; 
     }
+
+    function returnDataSet(d, desired_region){
+        var listing_list = []
+        
+        // Loop
+        for (var i=0; i<d.length; i++){
+            var current_listing = d[i];
+            if ((current_listing["Region"] == desired_region)) {
+                listing_list.push(current_listing);
+            }
+        }
+        
+        return listing_list;
+    }
+
+    // HELPER FUNCTION: Draw axes for the available
+    // listings graphs.
+    function drawListAxes(data, x, y, target_vis){
+
+        // Scale and axes definitions
+    
+        var xAxis = d3.svg.axis()
+                    .ticks(d3.time.years)
+                    .scale(x);
+
+        var yAxis = d3.svg.axis()
+                    .scale(y)
+                    .ticks(population_ticks)
+                    .orient('left')
+                    .tickPadding(8);
+
+        // Append axes        
+        target_vis.append("svg:g")
+        .attr("class", "axis")
+        .attr("transform", "translate(0," + (HEIGHT - MARGINS.bottom) + ")")
+        .call(xAxis);
+        
+        target_vis.append("svg:g")
+        .attr("class", "axis")
+        .attr("transform", "translate(" + (MARGINS.left) + ",0)")
+        .call(yAxis)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Active Listings");
+
+    }
+
+     function visAppend(region, data_set, target_vis, x, y, width) {
+
+        // HELPER FUNCTION for drawing lines
+        var lineGen = d3.svg.line()
+                .x(function(d) {
+                    return x(d.Date);
+                })
+                .y(function(d) {
+                    return y(d.Population);
+                })
+                .interpolate("monotone"); // Interpolate gives us curves
+
+        target_vis.append('svg:path')
+            .attr('class', region + ' data-line ')
+            .attr('d', lineGen(data_set))
+            .attr('stroke-width', width)
+            .attr('fill', 'none');
+
+        // Adding dots
+        target_vis.selectAll("g.dot."+region)
+        .data(data_set)
+        .enter().append("g")
+        .attr("class", "dot " + region)
+        .append("circle")
+        .attr("r", 3)
+        .attr("cx", function(d,i) { return x(d.Date); })
+        .attr("cy", function(d,i) { return y(d.Population); })
+    }
+
 
     // Handles the population graph. Currently in a 
     // col-md-6 block in the HTML.
-    function drawPopGraph(){
+    function drawPopGraph(svg, points, dates)
+    {
+        var start_date = dates[0];
+        var end_date = dates.pop();
+
+        WIDTH = $("#population").width();
+        HEIGHT = $("#population").height();
+
+        var vis = d3.select("#population_vis")
+        .attr("width", WIDTH)
+        .attr("height", HEIGHT);
+
+        var xScale = d3.time.scale()
+                    .domain([start_date, end_date])
+                    .range([MARGINS.left, WIDTH - MARGINS.right]);
+        var yScale = d3.scale.linear()
+                    .domain([0, d3.max(points, function(d) { return d.Population; })])
+                    .range([HEIGHT - MARGINS.top - MARGINS.bottom, 0]);
+
+        var north_pop = returnDataSet(points, "North");
+        north_pop.sort(sortObjByDate);
+        
+        var central_pop = returnDataSet(points, "Central");
+        central_pop.sort(sortObjByDate);
+
+        var south_pop = returnDataSet(points, "South");
+        south_pop.sort(sortObjByDate);
+
+        // Scale and axes definitions
+        drawListAxes(points, xScale, yScale,vis);
+
+        // Draw the lines
+        visAppend("North", north_pop, vis, xScale, yScale, line_weight);
+        visAppend("Central", central_pop, vis, xScale, yScale, line_weight);
+        visAppend("South", south_pop, vis, xScale, yScale, line_weight);
 
     }
 
@@ -322,8 +442,12 @@ function population(){
             console.error("Something went wrong fetching the population csv: " + error);
         }else{ 
             // Select only the correct svg
+
+            var date_array = getDataRange(points, "Date");
+            date_array.sort(dateSortAsc);
+
             var population_svg = d3.select("svg#population_vis");
-            drawPopGraph(population_svg, points);
+            drawPopGraph(population_svg, points, date_array);
         }
     });
 }
