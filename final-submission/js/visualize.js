8d0002bc47dd68c9cef03c41f7c325f1ae9414ef
Jeremy <y2kfirzen@gmail.com>
2016-04-13 15:30:03 -0700

    Cleaned up excess files from past assignments

--- final-submission/js/visualize.js
+++ final-submission/js/visualize.js
@@ -42,7 +42,6 @@ function sortObjByDate(a,b) {
 }
 
 // Convert a number to a string with commas
-// Pulled from http://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
